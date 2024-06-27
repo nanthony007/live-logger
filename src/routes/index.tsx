a1a8d9042daa25af68262c37913d2e7f015f2463
Nick Anthony <nanthony007@gmail.com>
2024-06-26 21:55:39 -0700

    Fix intro, realign title so more tbale space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,12 @@ import MessageView from "~/components/MessageView";
 export default function Home() {
 	return (
 		<main class="text-center mx-auto text-sky-700 p-4 font-thin">
-			<h1 class="text-6xl uppercase my-16 underline underline-offset-auto">Live Logger</h1>
-			<div class="text-center mx-4 py-5">
+			<h1 class="text-6xl uppercase py-4 underline underline-offset-auto">Live Logger</h1>
+			<div class="text-center mx-4 py-2">
 				<p>Welcome to the Live Logger application!</p>
 				<p>
 					Click the <span class="font-bold">"Download"</span> button to download the log as a CSV
-					file.
-				</p>
-				<p>
-					Add a <span class="italic">"File Name"</span> to customize your downloaded filename
-					<span class="font-thin">(default="log-messages.csv")</span>.
+					file. Add a <span class="italic">"File Name"</span> to customize your downloaded filename.
 				</p>
 			</div>
 
