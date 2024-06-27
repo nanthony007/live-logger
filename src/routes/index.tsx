a7f66e2e1ece6e1d02a5f054804def237df74ab3
Nick Anthony <nanthony007@gmail.com>
2024-06-26 21:42:01 -0700

    Update intro paragraph

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,11 +7,12 @@ export default function Home() {
 			<div class="text-center mx-4 py-5">
 				<p>Welcome to the Live Logger application!</p>
 				<p>
-					Click the <strong>"Download"</strong> button to download the log as a CSV file.
+					Click the <span class="font-bold">"Download"</span> button to download the log as a CSV
+					file.
 				</p>
-				<p class="italic">
-					Add a <strong>"Log Title"</strong> to customize your downloaded filename
-					(default="log-messages.csv").
+				<p>
+					Add a <span class="italic">"File Name"</span> to customize your downloaded filename
+					<span class="font-thin">(default="log-messages.csv")</span>.
 				</p>
 			</div>
 
