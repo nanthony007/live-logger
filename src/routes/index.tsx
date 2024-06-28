52dd69256fdbefa7f6253bbb77fd4a5370dffacf
Nick Anthony <nanthony007@gmail.com>
2024-06-28 11:08:00 -0700

    Manually Switch/Match, remove Dynamic, remove arrow funcs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import { Dynamic } from "solid-js/web";
 
 import { TabSection } from "~/components/TabSection";
 import { LogView } from "~/components/LogView";
-import { JSX, createMemo, onMount } from "solid-js";
+import { JSX, Match, Switch, createMemo, onMount } from "solid-js";
 
 type PageStore = {
 	tabCount: number;
 	selectedTab: number;
-	tabOptions: Array<() => JSX.Element>;
+	tabOptions: Array<JSX.Element>;
 };
 
 export default function Home() {
@@ -18,14 +18,14 @@ export default function Home() {
 		tabOptions: [],
 	});
 	onMount(() => {
-		setStore("tabOptions", store.tabOptions.length, () => <LogView />);
+		setStore("tabOptions", store.tabOptions.length, <LogView />);
 	});
-	const currentTab = createMemo(() =>
-		store.tabOptions.find((_, idx) => idx == store.selectedTab - 1)
-	);
+	// const currentTab = createMemo(() =>
+	// 	store.tabOptions.find((_, idx) => idx == store.selectedTab - 1)
+	// );
 	function incrementTabCount() {
 		setStore("tabCount", store.tabCount + 1);
-		setStore("tabOptions", store.tabOptions.length, () => <LogView />);
+		setStore("tabOptions", store.tabOptions.length, <LogView />);
 	}
 	function decrementTabCount() {
 		// if selected tab is last tab, decrement selected tab
@@ -48,7 +48,13 @@ export default function Home() {
 				selectedHandler={(idx) => setStore("selectedTab", idx)}
 			/>
 
-			<Dynamic component={currentTab()} />
+			<Switch fallback={<div class="text-red-500">404</div>}>
+				<Match when={store.selectedTab == 1}>{store.tabOptions[0]}</Match>
+				<Match when={store.selectedTab == 2}>{store.tabOptions[1]}</Match>
+				<Match when={store.selectedTab == 3}>{store.tabOptions[2]}</Match>
+			</Switch>
+
+			{/* <Dynamic component={currentTab()} /> */}
 		</main>
 	);
 }
